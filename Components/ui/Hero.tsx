03ea2e51c689be5780e1429bb5e4595ce98a25a1
Lucas Winkler <lucas.winkler@regalrexnord.com>
2024-05-03 21:16:06 -0500

    click event issue with prev and next buttons

--- Components/ui/Hero.tsx
+++ Components/ui/Hero.tsx
@@ -14,14 +14,14 @@ import { Card, CardContent } from "@/Components/ui/card";
 
 const Hero = () => {
   const [top10, setTop10] = useState<any[]>([]);
-  const [currentMovieIndex, setCurrentMovieIndex] = useState(8);
+  const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
 
   useEffect(() => {
     const fetchTop10 = async () => {
       try {
         const movies = await getTop10();
 
-        if(movies && movies.results && Array.isArray(movies.results)){
+        if(Array.isArray(movies?.results)){
           setTop10(movies.results.slice(0, 10));
         } else {
           console.error('Data is not an array:', movies);
@@ -35,28 +35,26 @@ const Hero = () => {
 
   }, []);
 
-  console.log(top10);
-
   function handlePreviousClick() {
-    setCurrentMovieIndex((prevIndex) => (prevIndex === 0 ? top10.length : prevIndex - 1));
+    setCurrentMovieIndex((prevIndex) => (prevIndex === 0 ? top10.length-1 : prevIndex - 1));
   }
   
   function handleNextClick() {
-    setCurrentMovieIndex((prevIndex) => (prevIndex === top10.length ? 0 : prevIndex + 1));
+    setCurrentMovieIndex((prevIndex) => (prevIndex === top10.length-1 ? 0 : prevIndex + 1));
   }
 
   return (
     <div
-      style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${top10[currentMovieIndex].backdrop_path})`, backgroundPosition: "center" }}
+      style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${top10[currentMovieIndex]?.backdrop_path})`, backgroundPosition: "center" }}
       className="w-full h-[1000px] bg-cover bg-center"
     > 
       <div className="container mx-auto pt-60 text-6xl text-white font-bold flex flex-col gap-6">
-        <h1>{top10[currentMovieIndex].original_title}</h1>
+        <h1>{top10[currentMovieIndex]?.original_title}</h1>
         <div className="flex flex-row gap-4 items-center">
           <span className="px-4 py-1 bg-amber-700 text-black text-xl rounded h-fit">
             IMDB
           </span>
-          <span className="text-3xl font-medium">{top10[currentMovieIndex].vote_average} / 10</span>
+          <span className="text-3xl font-medium">{top10[currentMovieIndex]?.vote_average} / 10</span>
         </div>
         <Carousel
           opts={{
@@ -71,17 +69,17 @@ const Hero = () => {
             {top10.map((movie: any, index: number) => (
               <CarouselItem key={index} className="basis-1/3">
                 <div className="p-1">
-                  <Card style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.poster_path})`, backgroundPosition: "center" }} className="bg-cover bg-center w-full">
-                  <CardContent>
-                    {index}
-                  </CardContent>
+                  <Card style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.poster_path})`, backgroundPosition: "center" }} className="bg-cover bg-center w-full">
+                    <CardContent>
+                      {index}
+                    </CardContent>
                   </Card>
                 </div>
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious onClick={handlePreviousClick}/>
-          <CarouselNext onClick={handleNextClick}/>
+          <CarouselPrevious />
+          <CarouselNext />
         </Carousel>
       </div>
     </div>
