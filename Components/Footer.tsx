7045ae5686e52b09728f4121c4c95e31088a1ca7
RBC\220662279 <Lucas.Winkler@regalrexnord.com>
2024-08-31 14:20:56 -0500

    footer tweaks

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -3,28 +3,33 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="container mx-auto flex flex-col gap-8 py-24 text-center">
+    <footer className="container mx-auto flex flex-col gap-8 pt-24 pb-12 text-center">
       <div className="relative flex items-center justify-center">
-        <span className="absolute left-0 top-1/2 w-[25%] md:w-[40%] -translate-y-1/2 transform border-t border-white"></span>
-        <ul className="z-10 flex gap-4">
+        <span className="absolute left-0 top-1/2 w-[25%] -translate-y-1/2 transform border-t border-white md:w-[40%]"></span>
+        <ul className="z-10 flex gap-8">
           <li>
-            <Github size={40} color="white" />
+            <Link href="https://github.com/LucasMERN" className="group">
+              <Github size={40} color="white" className="group-hover:scale-125" />
+            </Link>
           </li>
           <li>
-            <Linkedin size={40} color="white" />
+            <Link href="https://www.linkedin.com/in/lucaswinklerdev/" className="group">
+              <Linkedin size={40} color="white" className="group-hover:scale-125" />
+            </Link>
           </li>
         </ul>
-        <span className="absolute right-0 top-1/2 w-[25%] md:w-[40%] -translate-y-1/2 transform border-t border-white"></span>
+        <span className="absolute right-0 top-1/2 w-[25%] -translate-y-1/2 transform border-t border-white md:w-[40%]"></span>
       </div>
       <h3 className="text-xl font-medium text-white">
         Designed & Developed by{" "}
-        <Link href="#" className="underline decoration-dashed decoration-1 underline-offset-4">
+        <Link
+          href="#"
+          className="underline decoration-dashed decoration-1 underline-offset-4"
+        >
           Lucas Winkler
         </Link>
       </h3>
-      <h4 className="text-white -mt-4">
-        Copyright © 2024 Lucas Winkler
-      </h4>
+      <h4 className="-mt-4 text-white">Copyright © 2024 Lucas Winkler</h4>
     </footer>
   );
 };
