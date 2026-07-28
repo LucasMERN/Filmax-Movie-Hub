70d6da11294a44df67a8fcc5cabf899a8b97522b
DESKTOP-NG0GO5P\winkl <winkler.lucas1995@gmail.com>
2026-07-28 15:38:51 -0500

    next/shad upgrades

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -8,24 +8,24 @@ export function cn(...inputs: ClassValue[]) {
 export const fetchData = async (url: string) => {
   try {
     let res = await fetch(url, {
-      method: "GET",
+      method: 'GET',
       headers: {
-        accept: "application/json",
+        accept: 'application/json',
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
       },
       next: { revalidate: 30600 },
     });
 
     if (!res.ok) {
-      throw new Error("Failed to fetch data");
+      throw new Error('Failed to fetch data');
     }
 
     const data = await res.json();
     return data;
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error('Error fetching data:', error);
     throw error;
   }
 };
 
-export const apiUrl = "https://api.themoviedb.org/3";
+export const apiUrl = 'https://api.themoviedb.org/3';
